562d790f58ee711c0072c6ae254e9dc9cb88c27d
0x7c48 <0x7c48@gmail.com>
2017-03-25 15:07:44 +0200

    onyx-cassandra

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject onyx-cassandra "0.5.4"
+  :description "Onyx plugin for cassandra"
+  :url "FIX ME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+				 [org.onyxplatform/onyx "0.9.11"]
+                 [clojurewerkz/cassaforte "2.0.0"]]
+  :profiles {:dev {:dependencies []
+                   :plugins []}})
---
+++ src/onyx/plugin/cassandra.clj
@@ -0,0 +1,81 @@
+(ns onyx.plugin.cassandra
+    (:require [onyx.peer.task-lifecycle-extensions :as l-ext]
+              [onyx.peer.pipeline-extensions :as p-ext]
+              [clojurewerkz.cassaforte.client :as cass]
+              [clojurewerkz.cassaforte.cql :as cql]
+              [clojurewerkz.cassaforte.query :refer [paginate token]]))
+
+(defmethod l-ext/inject-lifecycle-resources
+  :cassandra/read-rows
+  [_ {:keys [onyx.core/task-map]  :as pipeline}]
+  {:cassandra/last-key (atom false)
+   :cassandra/session (cass/connect (:cassandra/hosts task-map) (:cassandra/keyspace task-map))})
+
+(defmethod l-ext/inject-lifecycle-resources
+  :cassandra/write-rows
+  [_ {:keys [onyx.core/task-map]  :as pipeline}]
+  {:cassandra/last-key (atom false)
+   :cassandra/session (cass/connect (:cassandra/hosts task-map) (:cassandra/keyspace task-map))})
+
+(defmethod p-ext/read-batch [:input :cassandra]
+  [{:keys [onyx.core/task-map onyx.core/task cassandra/last-key cassandra/session] :as pipeline}]
+
+  (cond
+    (nil? @last-key)
+    {:onyx.core/batch [{:input task :message :done}]}
+
+    :else
+    (let [opts (assoc (:cassandra/scan-options task-map) :limit (:onyx/batch-size task-map))
+          lk @last-key
+          opts (if lk (assoc opts :where [[> (token (:cassandra/partition-key task-map)) (token lk)]]) opts)
+          result
+          (cql/select
+            session
+            (:cassandra/table task-map)
+            opts)
+          last-key-in-set (if result ((:cassandra/partition-key task-map) (last result)))]
+      (reset! last-key last-key-in-set)
+      {:onyx.core/batch
+       (map (fn [r] {:input task :message r}) result)})))
+
+(defmethod p-ext/decompress-batch [:input :cassandra]
+  [{:keys [onyx.core/batch] :as event}]
+  {:onyx.core/decompressed (filter identity (map :message batch))})
+
+(defmethod p-ext/strip-sentinel [:input :cassandra]
+  [{:keys [onyx.core/decompressed]}]
+  {:onyx.core/tail-batch? (= (last decompressed) :done)
+   :onyx.core/requeue? false
+   :onyx.core/decompressed (remove (partial = :done) decompressed)})
+
+(defmethod p-ext/apply-fn [:input :cassandra]
+  [{:keys [onyx.core/decompressed]}]
+  {:onyx.core/results decompressed})
+
+(defmethod p-ext/apply-fn [:output :cassandra]
+  [_] {})
+
+(defmethod l-ext/close-lifecycle-resources
+  :cassandra/read-rows
+  [_ {:keys [cassandra/session] :as pipeline}]
+  (cass/disconnect session)
+  {})
+
+(defmethod l-ext/close-lifecycle-resources
+  :cassandra/write-rows
+  [_ {:keys [cassandra/session] :as pipeline}]
+  (cass/disconnect session)
+  {})
+
+(defmethod p-ext/compress-batch [:output :cassandra]
+  [{:keys [onyx.core/decompressed] :as pipeline}]
+  {:onyx.core/compressed decompressed})
+
+(defmethod p-ext/write-batch [:output :cassandra]
+  [{:keys [onyx.core/compressed onyx.core/task-map cassandra/session] :as pipeline}]
+  (when-not (empty? compressed)
+    (cql/insert-batch
+      session
+      (:cassandra/table task-map)
+      compressed))
+  {:onyx.core/written? true})
